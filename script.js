7017f8e606cfbdf64140ad6abcb4651cfae7eaab
Gabriel Vasconcelos <zchronoz@gmail.com>
2020-10-07 20:49:55 -0300

    adjust script

--- script.js
+++ script.js
@@ -3,14 +3,53 @@ $("#form-pesquisa").submit(function( event ) {
  
   event.preventDefault();
 
-  const uf = document.getElementById("cb-estado").value;
-  const style = document.getElementById("tipo-contrato").value;
-  const level = document.getElementById("senioridade").value;
-  const remote = document.getElementById("remoto").checked;
+  let uf = document.getElementById("cb-estado");
+  let style = document.getElementById("tipo-contrato");
+  let level = document.getElementById("senioridade");
+  let remote = document.getElementById("remoto").checked;
+  
+  let parameters = '';
+
+  if(uf.selectedIndex > 1 || style.selectedIndex > 1 || level.selectedIndex > 1){
+    parameters = '?labels=';
+
+    if (uf.selectedIndex > 1){
+      console.log('uf');
+      console.log(uf.selectedIndex);
+      parameters += uf.value + ',';
+    }
+
+    if (style.selectedIndex > 1){
+      console.log('style');
+      parameters += style.value + ',';
+    }
+
+    if (level.selectedIndex > 1){
+      console.log('level');
+      parameters += level.value + ',';
+    }
+
+    parameters = parameters.slice(0, -1);
+
+  }
+
+  remote= remote ? 'remoto' : ''
+
+  const url = "https://api.github.com/repos/frontendbr/vagas/issues" + parameters;
+
+  console.log(url);
 
   $.get({
-    url: "https://devjobs-dh.herokuapp.com/jobs/",
+    url: url,
     success: function(result){
+
+    $("#container-list-vagas").empty();
+
+
+    if (result.length === 0) {
+      $("#container-list-vagas").append('<h3>Não há resultados</h3>');
+    }
+
 
     const html = result.map(vaga => (
       `<div class="col mb-1">
@@ -31,7 +70,7 @@ $("#form-pesquisa").submit(function( event ) {
               <p>Salário: </p>
             </div>
 
-            <a id="saiba-mais" href="${vaga.html_url}">+</a>
+            <a id="saiba-mais" href="${vaga.html_url}" target="_blank">+</a>
           </div>
         </div>
       </div>
@@ -39,9 +78,6 @@ $("#form-pesquisa").submit(function( event ) {
     ));
 
     $("#container-list-vagas").append(html);
-
-    console.log(html);
-    
 
   }});
 
